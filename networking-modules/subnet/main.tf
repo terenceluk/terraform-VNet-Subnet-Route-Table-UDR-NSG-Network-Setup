d5c4558fe1408bc39c88c9aa38a4dd70b6ccb46e
Terence Luk <88775938+terenceluk@users.noreply.github.com>
2025-07-10 22:01:35 -0400

    Support Subnet Delegation

--- networking-modules/subnet/main.tf
+++ networking-modules/subnet/main.tf
@@ -3,4 +3,14 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = var.address_prefixes
   resource_group_name  = var.resource_group_name
   virtual_network_name = var.virtual_network_name
+
+  dynamic "delegation" {
+    for_each = var.delegation != null ? [var.delegation] : []
+    content {
+      name = delegation.value.name
+      service_delegation {
+        name = delegation.value.service_delegation.name
+      }
+    }
+  }
 }
