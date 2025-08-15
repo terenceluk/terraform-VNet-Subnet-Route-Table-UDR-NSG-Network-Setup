03c8985f8c9550781c02c674cf1bb85f93a3d2e8
Terence Luk <88775938+terenceluk@users.noreply.github.com>
2025-08-15 05:50:27 -0400

    Update main.tf

--- networking-modules/vnet/main.tf
+++ networking-modules/vnet/main.tf
@@ -3,4 +3,5 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = var.address_space
   location            = var.location
   resource_group_name = var.resource_group_name
+  tags                = var.tags
 }
