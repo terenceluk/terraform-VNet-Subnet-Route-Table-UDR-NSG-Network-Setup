20aa86add40d75989af8b5c9977ae1d0cbcfd387
Terence Luk <88775938+terenceluk@users.noreply.github.com>
2025-08-15 05:48:36 -0400

    Update main.tf

--- networking-modules/nsg/main.tf
+++ networking-modules/nsg/main.tf
@@ -2,6 +2,7 @@ resource "azurerm_network_security_group" "nsg" {
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
+  tags                = var.tags
 
   dynamic "security_rule" {
     for_each = var.security_rules
@@ -12,9 +13,22 @@ resource "azurerm_network_security_group" "nsg" {
       access                     = security_rule.value.access
       protocol                   = security_rule.value.protocol
       source_address_prefix      = security_rule.value.source_address_prefix
-      destination_address_prefix = security_rule.value.destination_address_prefix
       source_port_range          = security_rule.value.source_port_range
       destination_port_range     = security_rule.value.destination_port_range
+
+      # Handle single prefix
+      destination_address_prefix = (
+        can(security_rule.value.destination_address_prefix) && security_rule.value.destination_address_prefix != null
+        ? security_rule.value.destination_address_prefix
+        : null
+      )
+
+      # Handle multiple prefixes
+      destination_address_prefixes = (
+        can(security_rule.value.destination_address_prefixes) && security_rule.value.destination_address_prefixes != null
+        ? toset(security_rule.value.destination_address_prefixes)
+        : null
+      )
     }
   }
 }
