7c06963661f3220e5fa235776de756f3cfdb389b
Terence Luk <88775938+terenceluk@users.noreply.github.com>
2025-08-15 05:48:51 -0400

    Update variables.tf

--- networking-modules/nsg/variables.tf
+++ networking-modules/nsg/variables.tf
@@ -21,9 +21,16 @@ variable "security_rules" {
     direction                  = string
     access                     = string
     protocol                   = string
-    source_port_range          = string
-    destination_port_range     = string
     source_address_prefix      = string
-    destination_address_prefix = string
+    destination_port_range     = string
+    source_port_range          = string
+    destination_address_prefix = optional(string)
+    destination_address_prefixes = optional(list(string))
   }))
+}
+
+variable "tags" {
+  description = "A map of tags to apply to the NSG."
+  type        = map(string)
+  default     = {}
 }
